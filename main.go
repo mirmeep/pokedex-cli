2631832bfcbd76b3778a9c30685c77b18afb6bcc
mirmeep <mirpeak@gmail.com>
2026-08-10 10:54:08 -0700

    Refactor repl logic.

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		word := cleanInput(input)[0]
-		fmt.Printf("Your command was: %s\n", word)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		word := words[0]
+		fmt.Printf("Your command was: %s\n", word)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
