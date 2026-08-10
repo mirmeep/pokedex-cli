7e0c0ec64a93c07acfbc2d050b478001eb4807c5
mirmeep <mirpeak@gmail.com>
2026-08-10 11:27:34 -0700

    Add repl commands.

--- repl.go
+++ repl.go
@@ -7,7 +7,44 @@ import (
 	"fmt"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand {
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: 		"exit",
+			description: "Exit the Pokedex",
+			callback: 	commandExit,
+		},
+	}	
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	commands := getCommands()
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for key, val := range commands {
+		fmt.Printf("%s: %s\n", key, val.description)
+	}
+	return nil
+}
+
 func startRepl() {
+	commands := getCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -17,7 +54,13 @@ func startRepl() {
 			continue
 		}
 		word := words[0]
-		fmt.Printf("Your command was: %s\n", word)
+
+		cmd, exists := commands[word]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		cmd.callback()
 	}
 }
 
